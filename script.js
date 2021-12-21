73826881819e8e342e5245c165ac2d1ed87cd0c2
Russ Karimov <rkarimov.gfx@gmail.com>
2021-12-21 15:38:17 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let menu = document.querySelector('#menu-bar');
+
+let navbar = document.querySelector('#navbar-ul');
+
+
+menu.onclick = () => {
+
+ menu.classList.toggle('fa-times');
+ navbar.classList.toggle('active');
+}
